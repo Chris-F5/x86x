f42008d9b496334037e5275f8956ae2c5b0d48ce
Christopher Lang <christopher.lang.256@gmail.com>
2025-02-16 00:02:34 +0000

    Add utils_getenv function

---
+++ utils.asm
@@ -0,0 +1,38 @@
+section .text
+    global utils_getenv
+
+; Searches environment variable list to find the value associated with a name.
+;
+; @param rdi environ
+; @param rsi Environment variable name.
+; @return Pointer to the value in the environment or 0 if there is no match.
+utils_getenv:
+    cmp rdi, 0
+    je .return_no_match
+.next_entry:
+    mov rax, [rdi] ; Environment variable string.
+    mov r9, rsi ; Pointer to scan target environment variable name.
+    cmp rax, 0
+    je .return_no_match
+    add rdi, 8
+.next_char:
+    mov r10b, [rax] ; Next environment variable character.
+    mov r11b, [r9] ; Next environment variable name character.
+    cmp r10b, 0
+    je .next_entry
+    cmp r10b, '='
+    jne .compare_chars
+    cmp r11b, 0
+    je .return_match
+.compare_chars:
+    cmp r10b, r11b
+    jne .next_entry
+    add rax, 1
+    add r9, 1
+    jmp .next_char
+.return_match:
+    add rax, 1
+    ret
+.return_no_match
+    mov rax, 0
+    ret
